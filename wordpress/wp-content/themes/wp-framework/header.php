281b3a6be6cb9299fa3b19230fb0a030b207cfbe
crazyyy <crazyyy@gmail.com>
2015-04-30 11:22:49 +0300

    https://github.com/crazyyy/wp-framework/issues/19

--- wordpress/wp-content/themes/wp-framework/header.php
+++ wordpress/wp-content/themes/wp-framework/header.php
@@ -1,45 +1,39 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
-	<!-- dns prefetch -->
-	<link href="http://www.google-analytics.com/" rel="dns-prefetch">
-	<!-- meta -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<!-- icons -->
-	<link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
-	<!-- css + javascript -->
-	<?php wp_head(); ?>
-	<script>
-	!function(){
-		// configure legacy, retina, touch requirements @ conditionizr.com
-		conditionizr()
-	}()
-	</script>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
+    <!-- dns prefetch -->
+    <link href="http://www.google-analytics.com/" rel="dns-prefetch">
+    <!-- meta -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <!-- icons -->
+    <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
+    <!-- css + javascript -->
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <!-- wrapper -->
 <div class="wrapper clearfix">
-	<!-- header -->
-	<header class="header inner clearfix" role="banner">
-		<!-- logo -->
-		<div class="logo">
-			<?php if ( is_front_page() && is_home() ){ } else { ?>
-				<a href="<?php echo home_url(); ?>">
-			<?php  } ?>
-				<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
-			<?php if ( is_front_page() && is_home() ){
-			} else { ?>
-				</a>
-			<?php } ?>
-		</div>
-		<!-- /logo -->
-		<!-- nav -->
-		<nav class="nav" role="navigation">
-			<?php wpeHeadNav(); ?>
-		</nav>
-		<!-- /nav -->
-	</header>
-	<!-- /header -->
+    <!-- header -->
+    <header class="header inner clearfix" role="banner">
+        <!-- logo -->
+        <div class="logo">
+            <?php if ( is_front_page() && is_home() ){ } else { ?>
+                <a href="<?php echo home_url(); ?>">
+            <?php  } ?>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
+            <?php if ( is_front_page() && is_home() ){
+            } else { ?>
+                </a>
+            <?php } ?>
+        </div>
+        <!-- /logo -->
+        <!-- nav -->
+        <nav class="nav" role="navigation">
+            <?php wpeHeadNav(); ?>
+        </nav>
+        <!-- /nav -->
+    </header>
+    <!-- /header -->
