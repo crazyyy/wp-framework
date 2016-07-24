6cdc62dd3115ff1185ac79dd10174c2c8c1a6f4c
crazyyy <crazyyy@gmail.com>
2016-07-24 11:57:59 +0300

    before project restart

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,6 @@ var requireDir = require('require-dir');
 
 global.devBuild = process.env.NODE_ENV !== 'production';
 
-requireDir('./lib/gulp/tasks', { recurse: true });
+requireDir('./lib/gulp/tasks', {
+  recurse: true
+});
