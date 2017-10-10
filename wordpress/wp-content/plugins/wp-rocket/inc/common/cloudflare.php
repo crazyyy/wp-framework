5228751c60568ecb09beca633b62b4ef6906fc5a
crazyyy <crazyyy@gmail.com>
2017-10-10 19:19:15 +0300

    update cache plugin

--- wordpress/wp-content/plugins/wp-rocket/inc/common/cloudflare.php
+++ wordpress/wp-content/plugins/wp-rocket/inc/common/cloudflare.php
@@ -1,61 +1,51 @@
 <?php
-defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 /**
  * Set Real IP from CloudFlare
  *
+ * @since 2.8.16 Uses CloudFlare API v4 to get CloudFlare IPs
  * @since 2.5.4
  * @source cloudflare.php - https://wordpress.org/plugins/cloudflare/
  */
-add_action( 'init', '__rocket_set_real_ip_cloudflare' , 1 );
-function __rocket_set_real_ip_cloudflare() {
-    global $is_cf;
+function rocket_set_real_ip_cloudflare() {
+	global $is_cf;
 
-    $is_cf = ( isset( $_SERVER["HTTP_CF_CONNECTING_IP"] ) ) ? true : false;
+	$is_cf = ( isset( $_SERVER['HTTP_CF_CONNECTING_IP'] ) ) ? true : false;
 
-	// only run this logic if the REMOTE_ADDR is populated, to avoid causing notices in CLI mode
-    if ( isset( $_SERVER["REMOTE_ADDR"] ) ) {
-		if ( strpos( $_SERVER["REMOTE_ADDR"], ":" ) === false ) {
+	if ( ! $is_cf ) {
+	    return;
+	}
 
-			$cf_ip_ranges = array(
-				"199.27.128.0/21",
-				"173.245.48.0/20",
-				"103.21.244.0/22",
-				"103.22.200.0/22",
-				"103.31.4.0/22",
-				"141.101.64.0/18",
-				"108.162.192.0/18",
-				"190.93.240.0/20",
-				"188.114.96.0/20",
-				"197.234.240.0/22",
-				"198.41.128.0/17",
-				"162.158.0.0/15",
-				"104.16.0.0/12",
-				"172.64.0.0/13"
-			);
+	// only run this logic if the REMOTE_ADDR is populated, to avoid causing notices in CLI mode.
+	if ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+		$cf_ips_values = rocket_get_cloudflare_ips();
+
+		if ( is_wp_error( $cf_ips_values ) || ! isset( $cf_ips_values->success ) || ! $cf_ips_values->success ) {
+			return;
+		}
+
+		if ( strpos( $_SERVER['REMOTE_ADDR'], ':' ) === false ) {
+			$cf_ip_ranges = $cf_ips_values->result->ipv4_cidrs;
+
 			// IPV4: Update the REMOTE_ADDR value if the current REMOTE_ADDR value is in the specified range.
 			foreach ( $cf_ip_ranges as $range ) {
-				if ( rocket_ipv4_in_range( $_SERVER["REMOTE_ADDR"], $range ) ) {
-					if ( $_SERVER["HTTP_CF_CONNECTING_IP"] ) {
-						$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+				if ( rocket_ipv4_in_range( $_SERVER['REMOTE_ADDR'], $range ) ) {
+					if ( $_SERVER['HTTP_CF_CONNECTING_IP'] ) {
+						$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
 					}
 					break;
 				}
 			}
 		}
 		else {
-			$cf_ip_ranges = array(
-				"2400:cb00::/32",
-				"2606:4700::/32",
-				"2803:f800::/32",
-				"2405:b500::/32",
-				"2405:8100::/32"
-			);
-			$ipv6 = get_rocket_ipv6_full($_SERVER["REMOTE_ADDR"]);
+			$cf_ip_ranges = $cf_ips_values->result->ipv6_cidrs;
+
+			$ipv6 = get_rocket_ipv6_full( $_SERVER['REMOTE_ADDR'] );
 			foreach ( $cf_ip_ranges as $range ) {
 				if ( rocket_ipv6_in_range( $ipv6, $range ) ) {
-					if ( $_SERVER["HTTP_CF_CONNECTING_IP"]) {
-						$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+					if ( $_SERVER['HTTP_CF_CONNECTING_IP'] ) {
+						$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
 					}
 					break;
 				}
@@ -63,39 +53,15 @@ function __rocket_set_real_ip_cloudflare() {
 		}
 	}
 
-    // Let people know that the CF WP plugin is turned on, except if white label is active
-    if ( ! headers_sent() ) {
-        if ( rocket_is_white_label() ) {
-            $powered_by = get_rocket_option( 'wl_plugin_name' );
-        } else {
-            $powered_by = 'WP Rocket ' . WP_ROCKET_VERSION;
-        }
-
-        header( "X-CF-Powered-By: " . $powered_by );
-    }
-}
-
-/**
- * Reporting Spam IP to CloudFlare
- *
- * @since 2.5.4
- */
-add_action( 'wp_set_comment_status', '__rocket_reporting_spam_ip_to_cloudflare', 1, 2 );
-function __rocket_reporting_spam_ip_to_cloudflare( $id, $status ) {
-	if ( $status == 'spam' ) {
-		$comment = get_comment( $id );
-
-		if ( !is_null( $comment ) ) {
-			$payload = array(
-				"a" 	=> $comment->comment_author,
-				"am" 	=> $comment->comment_author_email,
-				"ip" 	=> $comment->comment_author_IP,
-				"con" 	=> substr( $comment->comment_content, 0, 100 )
-			);
-
-			$payload = urlencode( json_encode( $payload ) );
-
-			$GLOBALS['rocket_cloudflare']->reporting_spam_ip( $payload );
+	// Let people know that the CF WP plugin is turned on, except if white label is active.
+	if ( ! headers_sent() ) {
+		if ( rocket_is_white_label() ) {
+			$powered_by = get_rocket_option( 'wl_plugin_name' );
+		} else {
+			$powered_by = 'WP Rocket ' . WP_ROCKET_VERSION;
 		}
+
+		header( 'X-CF-Powered-By: ' . $powered_by );
 	}
 }
+add_action( 'init', 'rocket_set_real_ip_cloudflare' , 1 );
