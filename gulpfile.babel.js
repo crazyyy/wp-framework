aa1a92646e680b2019ed698abf38a3ffd87a564f
Vitalii A <crazyyy@gmail.com>
2021-07-15 12:21:53 +0000

    small cleanup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,7 @@
 'use strict';
 /* if work with html set TRUE, else - FALSE */
-const htmlOWp = true;
+const htmlOWp = false;
 let env_prod = true;
-
-// TODO: add https://github.com/olegskl/gulp-stylelint
-// TODO: add https://github.com/morishitter/stylefmt
-// TODO: add https://github.com/johno/immutable-css
-// TODO: add h Autoprefixer https://github.com/postcss/autoprefixer
-// Stylelint https://github.com/stylelint/stylelint
-// Postcss-flexbugs-fixes https://github.com/luisrudge/postcss-flexbugs-fixes
 
 // TODO: retina display mixnis transfer from mixin
 
@@ -47,7 +40,6 @@ if (htmlOWp === true) {
     host: config.domain
   }
 }
-
 
 /* import dependencies */
 import config from 'config';
--- todo.md
+++ todo.md
@@ -1,4 +1,12 @@
-## Projects for investigae 
+## Projects for investigae
 
 https://www.npmjs.com/package/starter-project-cli
-https://www.npmjs.com/package/starter-project 
+https://www.npmjs.com/package/starter-project
+
+
+// TODO: add https://github.com/olegskl/gulp-stylelint
+// TODO: add https://github.com/morishitter/stylefmt
+// TODO: add https://github.com/johno/immutable-css
+// TODO: add h Autoprefixer https://github.com/postcss/autoprefixer
+// Stylelint https://github.com/stylelint/stylelint
+// Postcss-flexbugs-fixes https://github.com/luisrudge/postcss-flexbugs-fixes
