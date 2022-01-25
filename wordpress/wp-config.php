e6ecb391465d3ffc250022f5dea55f8a932b7647
Vitalii A <crazyyy@gmail.com>
2022-01-25 23:33:49 +0200

    update .htaccess and wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,15 +1,29 @@
 <?php
+define( 'WP_CACHE', false );
 
-define('DB_NAME', 'wings_wings');
-define('DB_USER', 'wings_wings');
-define('DB_PASSWORD', 'L+Bn#+dGvO!@BE7J');
-// define('DB_NAME', 'DBNAME');
-// define('DB_USER', 'DBUSER');
-// define('DB_PASSWORD', 'DBPASS');
+//Begin Really Simple SSL session cookie settings
+@ini_set('session.cookie_httponly', true);
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+//END Really Simple SSL
+
+
+define('DB_NAME', 'DBNAME');
+define('DB_USER', 'DBUSER');
+define('DB_PASSWORD', 'DBPASS');
 
 define('DB_HOST', 'localhost');
 
-define('WP_DEBUG', true);
+define('WP_MEMORY_LIMIT', '512M');
+
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
+
+//if( WP_DEBUG && WP_DEBUG_DISPLAY && (defined('DOING_AJAX') && DOING_AJAX) ){
+//  @ ini_set( 'display_errors', 1 );
+//}
+
 define('WPCF7_AUTOP', false);
 
 define('CACHE_READ_WHITELIST','_transient|posts WHERE ID IN|limit_login_'); // do not read from cache is sql contains these
@@ -32,11 +46,7 @@ define('SECURE_AUTH_SALT', 'DGqahU{$#{1])WF?2d1{+v4mWhES6`o@))*asdaGcCa(t,+j~0+j
 define('LOGGED_IN_SALT',   '@a*]7xfnT!asd$-,Cw{~{Y~j38>jv!,]v%tr6jVRrH2:A)asrty3sg&56yuYZ=j+k>u@6`M|A}');
 define('NONCE_SALT',       '6jyK<[n:Wbnl)`;q2E:eVhp:[ez<+=|-xPadysegg5435g4n?WzGdEIfHqrFjeqV#zl|(oWv<4');
 
-/* Это всё, дальше не редактируем. Успехов! */
-
-/** Абсолютный путь к директории WordPress. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Инициализирует переменные WordPress и подключает файлы. */
 require_once(ABSPATH . 'wp-settings.php');
