c14737310758ab5545f82a6862423807027c5e31
Vitalii A <crazyyy@gmail.com>
2023-04-19 09:41:47 +0300

    Install Wordfence Security

---
+++ wp-content/wflogs/config-transient.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:0:{}
---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1682491283;s:7:"authKey";s:64:"YZw@!UnZKfFWB>,*{@i&i53tp~FF]Aa]NE7]3F130P=%HLV#G:pq=FxZW00F5yS}";}
