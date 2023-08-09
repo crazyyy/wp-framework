ac8b8deb6e1a89a9a7d24243a1a894dac1094c17
Vitalii Antoniuk <crazyyy@gmail.com>
2023-08-10 00:39:21 +0300

    updates

--- wp-content/themes/wp-framework/inc/theme-customize.php
+++ wp-content/themes/wp-framework/inc/theme-customize.php
@@ -179,7 +179,7 @@ function wpeb_remove_admin_bar_comments_menu () {
     remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
   }
 }
-//add_action('init', 'wpeb_remove_admin_bar_comments_menu');
+add_action('init', 'wpeb_remove_admin_bar_comments_menu');
 
   /**
    * Customize login logo for /wp-login.php and /wp-admin
@@ -224,3 +224,17 @@ function wpeb_remove_admin_bar_comments_menu () {
     return false;
   }
   add_filter('show_admin_bar', 'wpeb_remove_admin_bar');
+
+  // To remove all the WordPress default items use the code given below.
+  function wpeb_remove_dashboard_meta() {
+    remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+  }
+  add_action( 'admin_init', 'wpeb_remove_dashboard_meta' );
